8e36711cc6b7841e54e7486c9018ca8109a6df1b
namnv2901 <namnv.2901@gmail.com>
2018-11-05 23:41:46 +0700

    servlet

---
+++ ServletTut/src/Controllers/LoginController.java
@@ -0,0 +1,52 @@
+package Controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("txtUsername");
+		String pass = request.getParameter("txtPassword");
+		PrintWriter out = response.getWriter();
+		if (username.equals("admin") && pass.equals("admin")) {
+			out.write("<h1> Welcome " + username +"</h1>");
+			request.getSession().setAttribute("userName", username);
+			response.sendRedirect("HomeController");
+		} else {
+			out.write("<h1> dang nhạp that bai</h1>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
